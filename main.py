0efafdb52b44104730f84e5781d2891d8dfd43b0
Patryk Zabkiewicz <zabkiewi@pl3mcl-528854.ttg.global>
2015-08-27 16:09:45 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Image Analisis Toolkit
+
+Provide couple of algorithms made in concept of multithread
+
+Supported algorithms:
+    - Threshold
+    - Region growing
+    - Contour and gradient
+    - Network flow and path finding
+
+"""
+
+if __name__ == "__main__":
+   # load an image
+
+   # do some testing here
+
+   # analyse the results
+
